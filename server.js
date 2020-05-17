7bf3e0b26af592bfc965edb315cd41f1f2d47a39
Siddhant Jain <siddhantjain15298@gmail.com>
2020-05-17 17:44:06 +0530

    creating db Schema in Models

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const  port = 800;
+const db = require('./config/mongoose');
+const User = require('./models/user');
+
+
+
 
 app.use(express.static('./assets'));
 app.set('layout extractStyles',true);
