084ef99584e03a7e34eafc3936be2a089802c1d4
Siddhant Jain <siddhantjain15298@gmail.com>
2020-05-17 18:53:02 +0530

    adding signin and signup form

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,15 @@ module.exports.profile = (req,res)=>{
         title : "User_Profile"
     })
 }
+
+module.exports.signUp = (req,res)=>{
+    res.render('user_sign_up',{
+        title: "Codial | Sign-Up"
+    })
+}
+
+module.exports.signIn = (req,res)=>{
+    res.render('user_sign_in',{
+        title: "Codial | Sign-In"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 
 router.get('/profile',userController.profile);
-
+router.get('/sign-in',userController.signIn);
+router.get('/sign-up',userController.signUp);
 
 module.exports = router;
