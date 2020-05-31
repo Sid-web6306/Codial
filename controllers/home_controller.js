9354684ba367ab925976c64062664ccf83b4f3c4
Siddhant Jain <siddhantjain15298@gmail.com>
2020-05-31 16:10:18 +0530

    displaying comments on posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,7 +4,17 @@ const Post = require('../models/post');
 module.exports.home = (req,res)=>{
     // console.log(req.cookies);
     // res.cookie('user_id',13);
-        Post.find({}, function(err,posts){
+        Post.find({})
+        .populate('user')
+        .populate(
+            {
+                path: 'comments',
+                populate: {
+                    path:'user'
+                }
+            }
+        )
+        .exec(function(err,posts){
             return res.render('home',{
             title: "Codial | home",
             posts: posts
