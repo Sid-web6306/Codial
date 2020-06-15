dc75749a5877596654c918aca9b5a7a3fd04a56b
Siddhant Jain <siddhantjain15298@gmail.com>
2020-06-15 14:01:48 +0530

    Adding nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+const ejs = require ('ejs');
+
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'siddhantjain15298@gmail.com', // generated ethereal user
+      pass: '9559817935', // generated ethereal password
+    },
+  });
+
+let renderTemplate = (data,relativePath)=>{
+    let mailHTML;
+    ejs.renderFile{
+        path.join(__dirname,'../views/mailers',relativePath),
+        data,
+        function(err,templte){
+            if(err){
+                console.log('error in rendering a template');return;
+            }
+        }
+        mailHTML = template;
+    }
+    return mailHTML;
+    
+}
+
+module.exports = {
+    transporter:transporter,
+    renderTemplate:renderTemplate
+}
