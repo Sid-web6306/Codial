a5957249085aab9386f4922fad15be2f988bb455
Siddhant Jain <siddhantjain15298@gmail.com>
2020-06-15 14:06:00 +0530

    Adding nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 const ejs = require ('ejs');
-
+const path = require('path');
 let transporter = nodemailer.createTransport({
     service:'gmail',
     host: 'smtp.gmail.com',
@@ -17,7 +17,7 @@ let renderTemplate = (data,relativePath)=>{
     ejs.renderFile{
         path.join(__dirname,'../views/mailers',relativePath),
         data,
-        function(err,templte){
+        (err,template)=>{
             if(err){
                 console.log('error in rendering a template');return;
             }
